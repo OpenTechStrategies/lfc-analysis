205cbfa50b66dfecf8c28dbeb7335cc197a73665
isabelwu6464 <isabelwu@college.harvard.edu>
2021-06-24 22:22:43 -0400

    Wrap code in functions

--- src/lfc_scripts.py
+++ src/lfc_scripts.py
@@ -3,11 +3,12 @@ import config
 from geopy.geocoders import Nominatim
 import folium
 
-def main():
-    COMPETITION_NAME = "LoneStar2020"
-    site = mwclient.Site('torque.leverforchange.org/', 'GlobalView/', scheme="https")
-    site.login(config.username, config.api_key)
+COMPETITION_NAME = "LFC100Change2020"
+site = mwclient.Site('torque.leverforchange.org/', 'GlobalView/', scheme="https")
+site.login(config.username, config.api_key)
+geolocator = Nominatim(user_agent = "map")
 
+def main():
     response = site.api(
         'torquedataconnect',
         format='json',
@@ -15,8 +16,17 @@ def main():
     )
     proposal_ids = response["result"]
 
-    geolocator = Nominatim(user_agent = "map")
+    locations = extract_locations(proposal_ids)
+    generate_map(locations)
 
+def generate_map(locations):
+    # Plot latlong points
+    mapit = folium.Map(location=[48, -102], zoom_start=6)
+    for coord in locations:
+        folium.Marker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=4).add_to(mapit)
+    mapit.save('map1.html')
+
+def extract_locations(proposal_ids):
     # Extract organization locations
     locations = []
     for id in proposal_ids:
@@ -27,18 +37,14 @@ def main():
             path='/competitions/' + COMPETITION_NAME + '/proposals' + '/' + id
         )
         response['result']
-        org_location = geolocator.geocode(concat_org_location(response['result'])) # Convert named address to latlong pair
+        org_location = geolocator.geocode(
+            concat_org_location(response['result']))  # Convert named address to latlong pair
         try:
             coordinate_pair = (org_location.latitude, org_location.longitude)
         except:
             print('error, not a location')
         locations.append(coordinate_pair)
-
-    # Plot latlong points
-    mapit = folium.Map(location = [48, -102], zoom_start=6)
-    for coord in locations:
-        folium.Marker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=4).add_to(mapit)
-    mapit.save('map.html')
+    return locations
 
 def concat_org_location(proposal):
 
