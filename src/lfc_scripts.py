e11cb221c11f54825b1be912c26627e96ae94f46
isabelwu6464 <55456923+isabelwu6464@users.noreply.github.com>
2021-07-12 15:13:48 -0400

    Update readme
    Readme reflects addition of command line arguments

--- src/lfc_scripts.py
+++ src/lfc_scripts.py
@@ -17,10 +17,9 @@ def main():
 
     #Setup Map
     mapit = folium.Map(location=[48, -102], zoom_start=6)
-    loc = 'Corpus Christi'
     title_html = '''
                  <h3 align="center" style="font-size:16px"><b>Applicant Location Maps</b></h3>
-                 '''.format(loc)
+                 '''.format()
 
     #Retrieve proposal ids from competition
     response = site.api(
@@ -34,14 +33,17 @@ def main():
 
     feature_group = FeatureGroup(competition)
     for coord in locations:
-        folium.CircleMarker(location=[coord[0], coord[1]], fill_color=color, radius=7, color = color, opacity = 0.5, weight = 1).add_to(feature_group)
+        folium.CircleMarker(location=[coord[0], coord[1]],
+                            fill_color=color, radius=7,
+                            color = color, opacity = 0.5,
+                            weight = 1).add_to(feature_group)
     feature_group.add_to(mapit)
     LayerControl().add_to(mapit)
     mapit.get_root().html.add_child(folium.Element(title_html))
     mapit.save('applicant_locations_' + competition + '.html')
 
 def extract_locations(proposal_ids, competition):
-    #Failed geolocation spreadsheet
+
     error_list = []
     # Extract organization locations
     locations = []
@@ -54,7 +56,7 @@ def extract_locations(proposal_ids, competition):
         )
         response['result']
         org_location = geolocator.geocode(
-            concat_org_location(response['result']))  # Convert named address to latlong pair
+            concat_org_location(response['result'])) # Convert named address to latlong pair
         try:
             coordinate_pair = (org_location.latitude, org_location.longitude)
             locations.append(coordinate_pair)
